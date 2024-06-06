04d539f0a0e1bd1fe575a4104f19e8683301d671
Santiago Bruno <soysantiagobruno@gmail.com>
2024-06-06 15:57:50 -0300

    se agrego bootstrap y componentes navbar y home

--- frontend-adoptapp/src/app/app.component.ts
+++ frontend-adoptapp/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'frontend-adoptapp';
