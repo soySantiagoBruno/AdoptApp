810c4b93843431180b7fa4ca98e5949e3817b946
Santiago Bruno <soysantiagobruno@gmail.com>
2024-06-06 12:26:37 -0300

    creacion componente navbar y comienzo de componente home

--- frontend-carpeta-angular/src/app/app.module.ts
+++ frontend-carpeta-angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
