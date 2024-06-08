98ff509276797a34c37b725880e4a5fde89edfb7
Santiago Bruno <soysantiagobruno@gmail.com>
2024-06-08 00:29:45 -0300

    creo componentes mascotas-adopcion y navbar-usuario

---
+++ frontend-adoptapp/src/app/mascotas-adopcion/mascotas-adopcion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascotas-adopcion',
+  standalone: true,
+  imports: [],
+  templateUrl: './mascotas-adopcion.component.html',
+  styleUrl: './mascotas-adopcion.component.css'
+})
+export class MascotasAdopcionComponent {
+
+}
