1c5f6c12db66207dc67e44e4be72bb8f55657a2a
Santiago Bruno <soysantiagobruno@gmail.com>
2024-06-07 23:49:19 -0300

    se hizo modal info-mascota

--- frontend-adoptapp/src/app/carrusel/carrusel.component.ts
+++ frontend-adoptapp/src/app/carrusel/carrusel.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { InfoMascotaComponent } from "../info-mascota/info-mascota.component";
 import { NgIf, NgStyle } from '@angular/common';
 import { SwitchService } from '../services/switch.service';
+import { CartaComponent } from "../carta/carta.component";
 
 @Component({
     selector: 'app-carrusel',
     standalone: true,
     templateUrl: './carrusel.component.html',
     styleUrl: './carrusel.component.css',
-    imports: [InfoMascotaComponent, NgIf, NgStyle]
+    imports: [InfoMascotaComponent, NgIf, NgStyle, CartaComponent]
 })
 export class CarruselComponent implements OnInit{
   
